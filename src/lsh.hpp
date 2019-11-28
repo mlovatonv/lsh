d9856855f81e298725b157bebbb1fcd9697b819b
Jorge Ignacio Fiestas Lavezzolo <jorge.fiestas@utec.edu.pe>
2019-11-28 18:40:41 -0500

    First version of LSH

--- src/lsh.hpp
+++ src/lsh.hpp
@@ -6,29 +6,39 @@
 #include "hash.hpp"
 #include "point.hpp"
 
-template <std::size_t N>
+template <std::size_t N, std::size_t L>
 class LSH {
 	private:
 		using Points = std::vector<Point<N>>;
 		using Region = std::unordered_map<unsigned long long, Points>;
-			
-		Points points;
-		Region regions;
-		std::vector<Hash<N>> hashes;
+		
+		std::size_t numOfPlanes
+		Region regions[L];
+		Hash<N> hashes[L];
 
 	public:
-		LSH (int dimension,int numOfPlanes, Points points) :
-			dimension(dimension), numOfPlanes(numOfPlanes) {
+		LSH (std::size_t numOfPlanes, Points points) :
+			numOfPlanes(numOfPlanes) {
 
-				for (int i = 0; i < N 
+				for (int i = 0; i < L; i++) {
+					Hash<N> tempHash(numOfPlanes);
+					hashes[i] = tempHash;
+				}
 
-				for (std::size_t i = 0; i < N; i++) {
+				for (Point  point : points) {
 					regions[hash(point)].emplace_back(point);
 				}
 			}
 
 		Points kNN (Point point) {
-			return regions[hash(point)];
+			Points nns;
+
+			for (Hash hash : hashes) { 
+				//TODO: Correct union/intersection function
+				nns = regions[hash(point)];
+			}
+
+			return nns;
 		}
 }
 
